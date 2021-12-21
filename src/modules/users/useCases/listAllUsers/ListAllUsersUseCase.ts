2da17c4d9385d5c3607140bf109c1e0695050327
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2021-12-20 22:26:55 -0300

    Initial commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,29 @@
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    // Complete aqui
+
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error('User not authorized');
+    }
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const users = this.usersRepository.list();
+    return users;
+  }
+}
+
+export { ListAllUsersUseCase };
